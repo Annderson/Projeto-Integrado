66fec2baa1825090d6b2b64c2bcdb9fa20aeb21d
Anderson <arn412@hotmail.com>
2016-11-04 14:35:31 -0200

    Renomendo pastas

---
+++ PI_D-Cartão_de_Credito/SiteCCredito/src/conta/Cliente.java
@@ -0,0 +1,68 @@
+package conta;
+
+import java.util.Date;
+
+/**
+ * Created by anderson on 28/10/16.
+ */
+
+/**
+ * Classe espelho do Bando de Dados, representando um cliente
+ */
+public class Cliente {
+
+    private Integer id_cliente;
+    private String nome;
+    private Integer idade;
+    private Date nascimento;
+    private String cpf;
+    private String endereço;
+
+    public Integer getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(Integer id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+}
---
+++ PI_D-Cartão_de_Credito/SiteCCredito/src/conta/Fatura.java
@@ -0,0 +1,10 @@
+package conta;
+
+/**
+ * Created by anderson on 28/10/16.
+ */
+public class Fatura {
+
+
+
+}
