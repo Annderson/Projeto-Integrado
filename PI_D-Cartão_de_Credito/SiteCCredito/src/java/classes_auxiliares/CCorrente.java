16301433bd8d5c11b15ddb2b3688323885798b92
Anderson <arn412@hotmail.com>
2016-11-06 09:42:58 -0200

    Pastas renomeadas

--- PI_D-Cartão_de_Credito/SiteCCredito/src/java/classes_auxiliares/CCorrente.java
+++ PI_D-Cartão_de_Credito/SiteCCredito/src/java/classes_auxiliares/CCorrente.java
@@ -5,6 +5,8 @@
  */
 package classes_auxiliares;
 
+import java.util.Objects;
+
 /**
  *Classe espelho, da entidade CCorrente do banco de dados.
  * Para ser auxiliar nos comandos de setar e obter dados do banco.
@@ -12,6 +14,86 @@ package classes_auxiliares;
  */
 public class CCorrente {
     
-    
+    private Long idConta;
+    private Long idCliente;
+    private String numero_conta;
+    private String codigo;
+    private String numero_cartao;
+    private Double total;
+
+    public Long getIdConta() {
+        return idConta;
+    }
+
+    public void setIdConta(Long idConta) {
+        this.idConta = idConta;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }    
+
+    public String getNumero_conta() {
+        return numero_conta;
+    }
+
+    public void setNumero_conta(String numero_conta) {
+        this.numero_conta = numero_conta;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNumero_cartao() {
+        return numero_cartao;
+    }
+
+    public void setNumero_cartao(String numero_cartao) {
+        this.numero_cartao = numero_cartao;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.idConta);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CCorrente other = (CCorrente) obj;
+        
+        if (!Objects.equals(this.idConta, other.idConta)) {
+            return false;
+        }
+        if (!Objects.equals(this.numero_conta, other.numero_conta)) {
+            return false;
+        }
+        return true;
+    }  
     
 }
--- PI_D-Cartão_de_Credito/SiteCCredito/src/java/classes_auxiliares/Fatura.java
+++ PI_D-Cartão_de_Credito/SiteCCredito/src/java/classes_auxiliares/Fatura.java
@@ -5,11 +5,87 @@
  */
 package classes_auxiliares;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *Classe espelho, da entidade Fatura do banco de dados.
  * Para ser auxiliar nos comandos de setar e obter dados do banco.
  * @author anderson
  */
 public class Fatura {
+    
+    private Long idFatura;
+    private Long numero_conta;
+    private Double valor;
+    private String local;
+    private Date data_compra;
+
+    public Long getIdFatura() {
+        return idFatura;
+    }
+
+    public void setIdFatura(Long idFatura) {
+        this.idFatura = idFatura;
+    }
+
+    public Long getNumero_conta() {
+        return numero_conta;
+    }
+
+    public void setNumero_conta(Long numero_conta) {
+        this.numero_conta = numero_conta;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Date getData_compra() {
+        return data_compra;
+    }
+
+    public void setData_compra(Date data_compra) {
+        this.data_compra = data_compra;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + Objects.hashCode(this.idFatura);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fatura other = (Fatura) obj;
+        if (!Objects.equals(this.idFatura, other.idFatura)) {
+            return false;
+        }
+        if (!Objects.equals(this.numero_conta, other.numero_conta)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
